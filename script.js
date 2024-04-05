b0950cef28757de4f58ce1cda58b4c48e48e444f
Jane Ndafia <ndafiajane@gmail.com>
2024-04-05 03:48:02 +0100

    Js Equqlity operations

---
+++ script.js
@@ -0,0 +1,32 @@
+const age = 18;
+
+if (age === 18)
+  //if a code has only one line you dont need this {}so we have
+  console.log(`you just became an adult`);
+// this = is the assignment equqlity
+//this == perform type equality like 18 == "18" is true
+//this === is the comparison operstion that returns a boolean i.e 18==="18" is false...one is number ,the other is a string
+
+if (age == 18);
+console.log(`you just became an adult`);
+
+const favourite = prompt("what's your favourite number");
+console.log(favourite);
+console.log(typeof favourite);
+
+//logic to check
+if (favourite == 23) {
+  console.log(`cool! 23 is an amazing number!`);
+}
+
+if (favourite === 23) {
+  console.log(`cool! 23 is an amazing number!`);
+} else if (`favoourite === 7`) {
+  console.log(`7 is also a cool number`);
+} else if (favourite === 9) {
+  console.log(`9 is also a cool number`);
+  23;
+} else {
+  console.log(`number is not 23 or 7 or 9`);
+}
+if (favourite !== 23) console.log(`why not 23?`);
